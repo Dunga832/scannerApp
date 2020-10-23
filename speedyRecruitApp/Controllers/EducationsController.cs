73415837bf14463bbe54f747fa7d917b2df9a9ca
Rolivhuwa Phophi <Rol.Phophi@dynamicdna.co.za>
2020-10-23 15:40:53 +0200

    first commit

---
+++ speedyRecruitApp/Controllers/EducationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using speedyRecruitApp.Models;
+
+namespace speedyRecruitApp.Controllers
+{
+    public class EducationsController : Controller
+    {
+        private ScannerDbEntities db = new ScannerDbEntities();
+
+        // GET: Educations
+        public ActionResult Index()
+        {
+            var educations = db.Educations.Include(e => e.Candidate);
+            return View(educations.ToList());
+        }
+
+        // GET: Educations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education);
+        }
+
+        // GET: Educations/Create
+        public ActionResult Create()
+        {
+            ViewBag.Candidate_Id = new SelectList(db.Candidates, "Candidate_Id", "Name");
+            return View();
+        }
+
+        // POST: Educations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Edu_Id,Date_Obtained,Qualification,Institution,Candidate_Id")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Educations.Add(education);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Candidate_Id = new SelectList(db.Candidates, "Candidate_Id", "Name", education.Candidate_Id);
+            return View(education);
+        }
+
+        // GET: Educations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Candidate_Id = new SelectList(db.Candidates, "Candidate_Id", "Name", education.Candidate_Id);
+            return View(education);
+        }
+
+        // POST: Educations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Edu_Id,Date_Obtained,Qualification,Institution,Candidate_Id")] Education education)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(education).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Candidate_Id = new SelectList(db.Candidates, "Candidate_Id", "Name", education.Candidate_Id);
+            return View(education);
+        }
+
+        // GET: Educations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Education education = db.Educations.Find(id);
+            if (education == null)
+            {
+                return HttpNotFound();
+            }
+            return View(education);
+        }
+
+        // POST: Educations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Education education = db.Educations.Find(id);
+            db.Educations.Remove(education);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ speedyRecruitApp/Controllers/QrcodeController.cs
@@ -0,0 +1,86 @@
+﻿using QRcodeDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+
+namespace QRcodeDemo.Controllers
+{
+    public class QrcodeController : Controller
+    {
+        // GET: Home
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Generate(Qrcode qrcode)
+        {
+            try
+            {
+                qrcode.QRCodeImagePath = GenerateQRCode(qrcode.QRCodeText);
+                ViewBag.Message = "QR Code Generated successfully";
+            }
+            catch (Exception ex)
+            {
+                //catch exception if there is any
+            }
+            return View("Index", qrcode);
+        }
+
+        private string GenerateQRCode(string qrcodeText)
+        {
+            string folderPath = "~/Images/";
+            string imagePath = "~/Images/QrCode.jpg";
+            // create new Directory if not exist
+            if (!Directory.Exists(Server.MapPath(folderPath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(folderPath));
+            }
+
+            var barcodeWriter = new BarcodeWriter();
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+            var result = barcodeWriter.Write(qrcodeText);
+
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeBitmap = new Bitmap(result);
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(barcodePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    barcodeBitmap.Save(memory, ImageFormat.Jpeg);
+                    byte[] bytes = memory.ToArray();
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            }
+            return imagePath;
+        }
+
+        public ActionResult Read()
+        {
+            return View(ReadQRCode());
+        }
+
+        private Qrcode ReadQRCode()
+        {
+            Qrcode barcodeModel = new Qrcode();
+            string barcodeText = "";
+            string imagePath = "~/Images/QrCode.jpg";
+            string barcodePath = Server.MapPath(imagePath);
+            var barcodeReader = new BarcodeReader();
+            //Decode the image to text
+            var result = barcodeReader.Decode(new Bitmap(barcodePath));
+            if (result != null)
+            {
+                barcodeText = result.Text;
+            }
+            return new Qrcode() { QRCodeText = barcodeText, QRCodeImagePath = imagePath };
+        }
+    }
+}
